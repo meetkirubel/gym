b733ecd7bafc5f6f0d8908d7591d9b936a806ef1
Kirubel Gizaw <meetkirubel@gmail.com>
2024-01-10 16:35:00 +0300

    update css

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Footer from '@components/Footer'
 import Header from '@components/Header'
 import '@style/globals.css'
+import '@style/all.css'
 export const metadata = {
   title: 'Power Gym',
   description: 'Power Gym Addis is a premier fitness facility located in Addis Ababa, Ethiopia  that opened in 1996 E.C. We provide a wide range of equipment, classes, and, personal training options to help our members achieve their health and fitness goals.',
